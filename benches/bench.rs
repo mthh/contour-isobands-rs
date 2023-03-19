fb6bfaa4b11122b2a5141e07d0aaa6a3319a1ea3
Matthieu Viry <matthieu.viry@univ-grenoble-alpes.fr>
2023-03-19 17:34:45 +0100

    Add more benchmark cases

--- benches/bench.rs
+++ benches/bench.rs
@@ -255,3 +255,96 @@ fn bench_contourbuilder_volcano_no_quadtree_with_xy_step_xy_origin_parallel(b: &
         )
     });
 }
+
+#[bench]
+#[cfg(feature = "parallel")]
+fn bench_contourbuilder_volcano_quadtree_without_xy_step_xy_origin_parallel(b: &mut Bencher) {
+    let data_str = include_str!("../tests/fixtures/volcano.json");
+    let raw_data: serde_json::Value = serde_json::from_str(data_str).unwrap();
+    let matrix: Vec<f64> = raw_data["data"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|x| x.as_f64().unwrap())
+        .collect();
+    let h = raw_data["height"].as_u64().unwrap() as usize;
+    let w = raw_data["width"].as_u64().unwrap() as usize;
+
+    b.iter(|| {
+        black_box(
+            ContourBuilder::new(w, h)
+                .use_quad_tree(true)
+                .par_contours(
+                    &matrix,
+                    &[
+                        90., 95., 100., 105., 110., 115., 120., 125., 130., 135., 140., 145., 150.,
+                        155., 160., 165., 170., 175., 180., 185., 190., 195., 200.,
+                    ],
+                )
+                .unwrap(),
+        )
+    });
+}
+
+#[bench]
+#[cfg(feature = "parallel")]
+fn bench_contourbuilder_volcano_quadtree_with_xy_step_xy_origin_parallel(b: &mut Bencher) {
+    let data_str = include_str!("../tests/fixtures/volcano.json");
+    let raw_data: serde_json::Value = serde_json::from_str(data_str).unwrap();
+    let matrix: Vec<f64> = raw_data["data"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|x| x.as_f64().unwrap())
+        .collect();
+    let h = raw_data["height"].as_u64().unwrap() as usize;
+    let w = raw_data["width"].as_u64().unwrap() as usize;
+
+    b.iter(|| {
+        black_box(
+            ContourBuilder::new(w, h)
+                .x_origin(100.)
+                .y_origin(100.)
+                .x_step(15.)
+                .y_step(15.)
+                .use_quad_tree(true)
+                .par_contours(
+                    &matrix,
+                    &[
+                        90., 95., 100., 105., 110., 115., 120., 125., 130., 135., 140., 145., 150.,
+                        155., 160., 165., 170., 175., 180., 185., 190., 195., 200.,
+                    ],
+                )
+                .unwrap(),
+        )
+    });
+}
+
+#[bench]
+fn bench_contourbuilder_volcano_quadtree_without_xy_step_xy_origin(b: &mut Bencher) {
+    let data_str = include_str!("../tests/fixtures/volcano.json");
+    let raw_data: serde_json::Value = serde_json::from_str(data_str).unwrap();
+    let matrix: Vec<f64> = raw_data["data"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|x| x.as_f64().unwrap())
+        .collect();
+    let h = raw_data["height"].as_u64().unwrap() as usize;
+    let w = raw_data["width"].as_u64().unwrap() as usize;
+
+    b.iter(|| {
+        black_box(
+            ContourBuilder::new(w, h)
+                .use_quad_tree(true)
+                .contours(
+                    &matrix,
+                    &[
+                        90., 95., 100., 105., 110., 115., 120., 125., 130., 135., 140., 145., 150.,
+                        155., 160., 165., 170., 175., 180., 185., 190., 195., 200.,
+                    ],
+                )
+                .unwrap(),
+        )
+    });
+}
